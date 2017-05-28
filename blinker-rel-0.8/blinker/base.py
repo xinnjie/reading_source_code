57973bf86ff755482e4cdd54521a3f5d3f7d6664
xinnjie <contact_xinjie@163.com>
2017-05-28 22:13:35 +0800

    add blinker

---
+++ blinker-rel-0.8/blinker/base.py
@@ -0,0 +1,234 @@
+# -*- coding: utf-8; fill-column: 76 -*-
+"""Signals and events.
+
+A small implementation of signals, inspired by a snippet of Django signal
+API client code seen in a blog post.  Signals are first-class objects and
+each manages its own receivers and message emission.
+
+The :func:`signal` function provides singleton behavior for named signals.
+
+"""
+from weakref import WeakValueDictionary
+
+from blinker._utilities import (
+    WeakTypes,
+    defaultdict,
+    hashable_identity,
+    reference,
+    symbol,
+    )
+
+
+ANY = symbol('ANY')
+ANY_ID = 0
+
+
+class Signal(object):
+    """A generic notification emitter."""
+
+    #: A convenience for importers, allows Signal.ANY
+    ANY = ANY
+
+    def __init__(self, doc=None):
+        if doc:
+            self.__doc__ = doc
+        self.receivers = {}
+        self._by_receiver = defaultdict(set)
+        self._by_sender = defaultdict(set)
+        self._weak_senders = {}
+
+    def connect(self, receiver, sender=ANY, weak=True):
+        """Connect *receiver* to signal events send by *sender*.
+
+        :param receiver: A callable.  Will be invoked by :meth:`send`.  Will
+          be invoked with `sender=` as a named argument and any \*\*kwargs
+          that were provided to a call to :meth:`send`.
+
+        :param sender: Any object or :attr:`Signal.ANY`.  Restricts
+          notifications to *receiver* to only those :meth:`send` emissions
+          sent by *sender*.  If ``ANY``, the receiver will always be
+          notified.  A *receiver* may be connected to multiple *sender* on
+          the same Signal.  Defaults to ``ANY``.
+
+        :param weak: If true, the Signal will hold a weakref to *receiver*
+          and automatically disconnect when *receiver* goes out of scope or
+          is garbage collected.  Defaults to True.
+
+        """
+        receiver_id = hashable_identity(receiver)
+        if weak:
+            receiver_ref = reference(receiver, self._cleanup_receiver)
+            receiver_ref.receiver_id = receiver_id
+        else:
+            receiver_ref = receiver
+        if sender is ANY:
+            sender_id = ANY_ID
+        else:
+            sender_id = hashable_identity(sender)
+        # 主要需要登记 receiver_id, sender_id， receiver_ref
+        # receiver_ref 对应两种情况 1. 当要求存入弱引用时(选项weak 为 true，需要对传入的引用做一次弱引用的封装)
+        #                          2. 选项为false时，直接赋值为原值就好了
+        # receiver的引用保存在 receivers dict 中，通过receiver_id进行查询
+
+        self.receivers.setdefault(receiver_id, receiver_ref)
+        # _by_sender 字典用来保存对应于每个sender的receiver 订阅者
+        # _by_receiver 字典相反，用来保存对于每个 receiver 订阅者的 sender
+        self._by_sender[sender_id].add(receiver_id)
+        self._by_receiver[receiver_id].add(sender_id)
+        # todo 这个del 很奇怪，每次函数结束receiver_ref是会被自动删除的才对，为什么要自行del
+        del receiver_ref
+
+        if sender is not ANY and sender_id not in self._weak_senders:
+            # wire together a cleanup for weakref-able senders
+            try:
+                sender_ref = reference(sender, self._cleanup_sender)
+                sender_ref.sender_id = sender_id
+            except TypeError:
+                pass
+            # 第一次碰到 try-except中的else。这个else对应于没有exception抛出的情况
+            else:
+                self._weak_senders.setdefault(sender_id, sender_ref)
+                del sender_ref
+
+        # broadcast this connection.  if receivers raise, disconnect.
+        if receiver_connected.receivers and self is not receiver_connected:
+            try:
+                receiver_connected.send(self,
+                                        receiver_arg=receiver,
+                                        sender_arg=sender,
+                                        weak_arg=weak)
+            except:
+                self.disconnect(receiver, sender)
+                raise
+        return receiver
+
+    def send(self, *sender, **kwargs):
+        """Emit this signal on behalf of *sender*, passing on \*\*kwargs.
+
+        Returns a list of 2-tuples, pairing receivers with their return
+        value. The ordering of receiver notification is undefined.
+
+        :param \*sender: Any object or ``None``.  If omitted, synonymous
+        with ``None``.  Only accepts one positional argument.
+
+        :param \*\*kwargs: Data to be sent to receivers.
+
+        """
+        # Using '*sender' rather than 'sender=None' allows 'sender' to be
+        # used as a keyword argument- i.e. it's an invisible name in the
+        # function signature.
+        if len(sender) == 0:
+            sender = None
+        elif len(sender) > 1:
+            raise TypeError('send() accepts only one positional argument, '
+                            '%s given' % len(sender))
+        else:
+            sender = sender[0]
+        if not self.receivers:
+            return []
+        else:
+            return [(receiver, receiver(sender, **kwargs))
+                    for receiver in self.receivers_for(sender)]
+
+    def has_receivers_for(self, sender):
+        """True if there is probably a receiver for *sender*.
+
+        Performs an optimistic check for receivers only.  Does not guarantee
+        that all weakly referenced receivers are still alive.  See
+        :meth:`receivers_for` for a stronger search.
+
+        """
+        if not self.receivers:
+            return False
+        if self._by_sender[ANY_ID]:
+            return True
+        if sender is ANY:
+            return False
+        return hashable_identity(sender) in self._by_sender
+
+    def receivers_for(self, sender):
+        """Iterate all live receivers listening for *sender*."""
+        # TODO: test receivers_for(ANY)
+        if self.receivers:
+            sender_id = hashable_identity(sender)
+            if sender_id in self._by_sender:
+                ids = (self._by_sender[ANY_ID] |
+                       self._by_sender[sender_id])
+            else:
+                ids = self._by_sender[ANY_ID].copy()
+            for receiver_id in ids:
+                receiver = self.receivers.get(receiver_id)
+                if receiver is None:
+                    continue
+                if isinstance(receiver, WeakTypes):
+                    strong = receiver()
+                    if strong is None:
+                        self._disconnect(receiver_id, ANY_ID)
+                        continue
+                    receiver = strong
+                yield receiver
+
+    def disconnect(self, receiver, sender=ANY):
+        """Disconnect *receiver* from this signal's events."""
+        if sender is ANY:
+            sender_id = ANY_ID
+        else:
+            sender_id = hashable_identity(sender)
+        receiver_id = hashable_identity(receiver)
+        self._disconnect(receiver_id, sender_id)
+
+    def _disconnect(self, receiver_id, sender_id):
+        if sender_id == ANY_ID:
+            if self._by_receiver.pop(receiver_id, False):
+                for bucket in self._by_sender.values():
+                    bucket.discard(receiver_id)
+            self.receivers.pop(receiver_id, None)
+        else:
+            self._by_sender[sender_id].discard(receiver_id)
+
+    def _cleanup_receiver(self, receiver_ref):
+        """Disconnect a receiver from all senders."""
+        self._disconnect(receiver_ref.receiver_id, ANY_ID)
+
+    def _cleanup_sender(self, sender_ref):
+        """Disconnect all receivers from a sender."""
+        sender_id = sender_ref.sender_id
+        assert sender_id != ANY_ID
+        self._weak_senders.pop(sender_id, None)
+        for receiver_id in self._by_sender.pop(sender_id, ()):
+            self._by_receiver[receiver_id].discard(sender_id)
+
+    def _clear_state(self):
+        """Throw away all signal state.  Useful for unit tests."""
+        self._weak_senders.clear()
+        self.receivers.clear()
+        self._by_sender.clear()
+        self._by_receiver.clear()
+
+
+receiver_connected = Signal()
+
+
+class NamedSignal(Signal):
+    """A named generic notification emitter."""
+
+    def __init__(self, name, doc=None):
+        Signal.__init__(self, doc)
+        self.name = name
+
+    def __repr__(self):
+        base = Signal.__repr__(self)
+        return "%s; %r>" % (base[:-1], self.name)
+
+
+class Namespace(WeakValueDictionary):
+
+    def signal(self, name, doc=None):
+        """Return the :class:`NamedSignal` *name*, creating it if required."""
+        try:
+            return self[name]
+        except KeyError:
+            return self.setdefault(name, NamedSignal(name, doc))
+
+
+signal = Namespace().signal
